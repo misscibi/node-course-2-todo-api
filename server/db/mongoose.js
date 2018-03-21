8c3d3766e4e48e232fa2ca07987d6ad3c4f803e0
CB <cnalangan@gmail.com>
2018-03-21 11:14:25 +0800

    Adding POST /todos route and refactor mongoose

---
+++ server/db/mongoose.js
@@ -0,0 +1,60 @@
+var mongoose = require('mongoose');
+
+
+// use the native Javascript Promise instead of Mongoose/third-party one
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+// mongoose waits for a connection first before it executes the queries
+// mongoose does typecasting
+
+module.exports = {mongoose};
+
+// var Todo = mongoose.model('Todo', {
+//     text: {
+//         type: String,
+//         required: true,
+//         minlength: 1,
+//         trim: true
+//     },
+//     completed: {
+//         type: Boolean,
+//         default: false
+//     },
+//     completedAt: {
+//         type: Number,
+//         default: null
+//     }
+// });
+
+// var newTodo = new Todo({
+//     text: 'Noctis to fish bass for dinner',
+//     completed: true,
+//     completedAt: 35
+// });
+//
+// // Saving to MongoDB database
+// newTodo.save().then((document) => {
+//     console.log('Saved todo', document);
+// }, (e) => {
+//     console.log('Unable to save todo')
+// });
+
+
+// var User = mongoose.model('User', {
+//     email: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         minlength: 1
+//     }
+// });
+//
+// var user = new User({
+//     email: 'link@hyrule.com'
+// });
+//
+// user.save().then((doc) => {
+//     console.log('User saved', doc);
+// }, (e) => {
+//     console.log('Unable to save user', e);
+// });
---
+++ server/server.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+// the bodyParser.json() return value is a function that will be used as middleware
+app.use(bodyParser.json());
+
+// Client will send a JSON object
+// Server will return a model
+app.post('/todos', (request, result) => {
+    // body gets stored by body-parser module
+    // console.log(request.body);
+    var todo = new Todo({
+        text: request.body.text,
+        completed: request.body.completed,
+        completedAt: request.body.completedAt
+    });
+
+    todo.save().then((document) => {
+        result.send(document);
+    }, (error) => {
+        result.status(400).send(error);
+    });
+});
+
+
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+});
+
+// var newTodo = new Todo({
+//     text: 'Noctis to fish bass for dinner',
+//     completed: true,
+//     completedAt: 35
+// });
+//
+// // Saving to MongoDB database
+// newTodo.save().then((document) => {
+//     console.log('Saved todo', document);
+// }, (e) => {
+//     console.log('Unable to save todo')
+// });
+
+
+// var user = new User({
+//     email: 'link@hyrule.com'
+// });
+//
+// user.save().then((doc) => {
+//     console.log('User saved', doc);
+// }, (e) => {
+//     console.log('Unable to save user', e);
+// });
